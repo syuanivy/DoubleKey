f9ed63ea291861f6a8d2e005751867eec34a7eb1
Terence Parr <parrt@antlr.org>
2014-08-13 17:27:21 -0700

    pull in junit in script

--- bild.py
+++ bild.py
@@ -13,7 +13,11 @@ if not os.path.exists("bilder.py"):
 # assumes bilder.py is in current directory
 from bilder import *
 
+def init():
+    download("http://search.maven.org/remotecontent?filepath=junit/junit/4.10/junit-4.10.jar", "/tmp")
+
 def compile():
+    require(init)
     javac("src", "out", cp="/tmp/junit-4.10.jar")
     javac("test", "out", cp="out:/tmp/junit-4.10.jar")
 
@@ -26,9 +30,7 @@ def test():
     junit('test', cp="dist/doublekey.jar:/tmp/junit-4.10.jar")
 
 def all():
-    #global JARCACHE
-    #JARCACHE='lib'
-    #mkdir('lib')
+    require(init)
     test()
 
 processargs(globals())
